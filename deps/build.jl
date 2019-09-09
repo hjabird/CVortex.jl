53cd7ea06b433f999c669af0cc1c8d484e1ba03a
HJA Bird <9040797+hjabird@users.noreply.github.com>
2019-09-09 17:53:49 +0100

    Bump to cvortex 0.3.4

--- deps/build.jl
+++ deps/build.jl
@@ -28,7 +28,7 @@
 using BinDeps
 
 # version of cvortex package to use
-cvortexver="0.3.3"
+cvortexver="0.3.4"
 
 # binaries url prefix
 url="https://github.com/hjabird/cvortex/releases/download/$cvortexver/"
